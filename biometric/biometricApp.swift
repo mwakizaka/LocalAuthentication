58d655a0814dbf25c33bedf1590b17c21adb3bbb
mwakizaka <21286384+mwakizaka@users.noreply.github.com>
2022-12-28 10:31:25 +0900

    Initial Commit

---
+++ biometric/biometricApp.swift
@@ -0,0 +1,17 @@
+//
+//  biometricApp.swift
+//  biometric
+//
+//  Created by mwakizaka on 2022/12/28.
+//
+
+import SwiftUI
+
+@main
+struct biometricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
