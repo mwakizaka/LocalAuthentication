c41fa00359864811ab7e29363f610064982a8af6
mwakizaka <21286384+mwakizaka@users.noreply.github.com>
2022-12-28 12:53:47 +0900

    feat: add biometric auth

--- biometric/ContentView.swift
+++ biometric/ContentView.swift
@@ -6,16 +6,36 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button(action: {
+                bitmetricAuthentication()
+            }){
+                Text("Authenticate with biometric")
+            }
         }
         .padding()
+    }
+    
+    func bitmetricAuthentication() {
+        let context = LAContext()
+        var error: NSError?
+        let description: String = "Authenticate"
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: description, reply: {success, evaluateError in
+                if (success) {
+                    print("Succeeded to authenticate")
+                } else {
+                    print("Failed to authenticate")
+                }
+            })
+        } else {
+            let errorDescription = error?.userInfo["NSLocalizedDescription"] ?? ""
+            print(errorDescription) // Biometry is not available on this device.
+        }
     }
 }
 
